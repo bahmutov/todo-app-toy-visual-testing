2702d4a6b97ee1357591b826f51063443c4111ff
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-01 19:32:12 -0400

    working on the solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,37 +3,8 @@
 it('adds and removes todos', () => {
   cy.visit('/')
   cy.get('[data-cy="add-todo"]').clear()
-  cy.get('[data-cy="add-todo"]').type(
-    'record the test{enter}',
-  )
-  cy.get('[data-cy="add-todo"]').clear()
-  cy.get('[data-cy="add-todo"]').type(
-    'find the secret{enter}',
-  )
-  cy.get('[data-cy="add-todo"]').clear()
-  cy.get('[data-cy="add-todo"]').type('use Studio{enter}')
-  cy.get('[data-cy=todo]').should('have.length', 3)
-
-  cy.imageDiff('00-added-todos')
-
-  cy.get('[data-cy=todo]')
-    .eq(1)
-    .find('.cb-input')
-    .check()
-    .should('be.checked')
-  cy.get('#items-left').should('have.text', '2')
-
-  cy.imageDiff('01-completed-todo')
-
-  cy.get('#completed').click()
-  cy.get('#completed').should('have.class', 'on')
-  cy.get('[data-cy=todo]:visible')
-    .should('have.length', 1)
-    .contains('find the secret')
-
-  cy.imageDiff('02-completed-view')
-
-  cy.get('button img[alt="Change color theme"]').click()
-
-  cy.imageDiff('03-night-view')
+  cy.get('[data-cy="add-todo"]')
+    .type('record the test')
+    .blur()
+  cy.imageDiff('typing-todo')
 })
