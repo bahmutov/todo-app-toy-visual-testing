d85331ae93e6c38f79cb5c4c917e4a8de899be8a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-01 19:15:39 -0400

    starting code

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,11 @@
 const { defineConfig } = require('cypress')
 // https://github.com/bahmutov/cypress-toy-visual-testing
-const {
-  setupVisualTesting,
-} = require('@bahmutov/cypress-toy-visual-testing/dist/plugin/setup')
+// configure the visual testing plugin
 
 module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {
-      setupVisualTesting(on, config)
       return config
     },
     baseUrl: 'http://localhost:3777',
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,1 @@
 // https://github.com/bahmutov/cypress-toy-visual-testing
-import '@bahmutov/cypress-toy-visual-testing/dist/support/commands'
