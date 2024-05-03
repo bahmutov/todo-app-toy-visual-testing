403c92cf79d10fb04b22e06241888a077846b8b2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-03 13:31:48 -0400

    g4

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,5 @@
-/// <reference types="cypress-axe" />
+/// <reference types="cypress" />
 
 it('uses application CSS styles', () => {
   cy.visit('/')
-  cy.get('input#addt').type('hello{enter}')
-  cy.get('[data-cy="todo"]')
-    .should('have.length', 1)
-    .first()
-    .find('input:checkbox')
-    .check()
-  cy.contains('button', 'Completed').click()
-  cy.contains('#items-left', '0')
-  cy.get('button [alt="Change color theme"]').click()
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,2 @@
 // https://github.com/bahmutov/cypress-toy-visual-testing
 import '@bahmutov/cypress-toy-visual-testing/dist/support/commands'
-
-// https://github.com/component-driven/cypress-axe
-import 'cypress-axe'
