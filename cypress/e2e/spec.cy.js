0c35f9c7d1187257f940c788405e781bd4db81be
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-03 09:51:00 -0400

    g3 solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,39 +1,14 @@
-/// <reference types="cypress" />
+/// <reference types="cypress-axe" />
 
-it('adds and removes todos', () => {
+it('has good text contrast', () => {
   cy.visit('/')
-  cy.get('[data-cy="add-todo"]').clear()
-  cy.get('[data-cy="add-todo"]').type(
-    'record the test{enter}',
-  )
-  cy.get('[data-cy="add-todo"]').clear()
-  cy.get('[data-cy="add-todo"]').type(
-    'find the secret{enter}',
-  )
-  cy.get('[data-cy="add-todo"]').clear()
-  cy.get('[data-cy="add-todo"]').type('use Studio{enter}')
-  cy.get('[data-cy=todo]').should('have.length', 3)
-
-  cy.imageDiff('00-added-todos')
-
-  cy.get('[data-cy=todo]')
-    .eq(1)
-    .find('.cb-input')
-    .check()
-    .should('be.checked')
-  cy.get('#items-left').should('have.text', '2')
-
-  cy.imageDiff('01-completed-todo')
-
-  cy.get('#completed').click()
-  cy.get('#completed').should('have.class', 'on')
-  cy.get('[data-cy=todo]:visible')
-    .should('have.length', 1)
-    .contains('find the secret')
-
-  cy.imageDiff('02-completed-view')
-
+  cy.imageDiff('day')
+  // start the a11y checks using cypress-axe plugin
+  // https://github.com/component-driven/cypress-axe
+  cy.injectAxe()
+  cy.checkA11y()
   cy.get('button img[alt="Change color theme"]').click()
-
-  cy.imageDiff('03-night-view')
+  cy.imageDiff('night')
+  // check the a11y again
+  cy.checkA11y()
 })
