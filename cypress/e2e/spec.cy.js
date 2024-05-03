464e82ca8cf942ab7b49ed89df0f79b82305c701
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-03 13:39:55 -0400

    covered

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,11 @@
 
 it('uses application CSS styles', () => {
   cy.visit('/')
+  cy.get('input#addt').type('hello{enter}')
+  cy.get('[data-cy=todo]')
+    .should('have.length', 1)
+    .first()
+    .find('input:checkbox')
+    .check()
+  cy.contains('#items-left', '0').should('be.visible')
 })
