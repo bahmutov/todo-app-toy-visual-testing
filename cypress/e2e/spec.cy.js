9432dfa4e26a8cee145cade252f1a5dfc7be1b9d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-06 10:42:53 -0400

    update comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,14 @@
 /// <reference types="cypress" />
 
-it('takes image diffs on CI while recording to Cypress Cloud', () => {
+it('takes an image diff of a tall page', () => {
   cy.visit('/')
+  // add 20 todos
+  // Tip: Lodash _.times is a good utility function
   Cypress._.times(20, (k) => {
     cy.get('input#addt').type(`todo ${k + 1}{enter}`)
   })
+  // confirm there are 20 todos on the page
   cy.get('[data-cy=todo]').should('have.length', 20)
-  cy.screenshot('00-one-todo', { overwrite: true })
-  cy.screenshot('01-the-runner', {
-    overwrite: true,
-    capture: 'runner',
-  })
-  cy.imageDiff('02-visual-diff')
+  // use image diff assertion
+  cy.imageDiff('20-todos')
 })
