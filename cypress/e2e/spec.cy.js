e6e4c7d7a6d5d51010174bb107002550dcb483d5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-06 08:43:55 -0400

    taking screenshots

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress" />
 
-it('uses application CSS styles', () => {
+it('takes image diffs on CI while recording to Cypress Cloud', () => {
   cy.visit('/')
   cy.get('input#addt').type('hello{enter}')
-  cy.get('[data-cy=todo]')
-    .should('have.length', 1)
-    .first()
-    .find('input:checkbox')
-    .check()
-  cy.contains('#items-left', '0').should('be.visible')
+  cy.get('[data-cy=todo]').should('have.length', 1)
+  cy.screenshot('00-one-todo', { overwrite: true })
+  cy.screenshot('01-the-runner', {
+    overwrite: true,
+    capture: 'runner',
+  })
+  cy.imageDiff('02-visual-diff')
 })
