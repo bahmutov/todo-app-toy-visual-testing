0dabf8237f496f301cc13a3bc9030205638fed9a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-06 09:14:11 -0400

    try recording again

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,8 +2,10 @@
 
 it('takes image diffs on CI while recording to Cypress Cloud', () => {
   cy.visit('/')
-  cy.get('input#addt').type('hello{enter}')
-  cy.get('[data-cy=todo]').should('have.length', 1)
+  Cypress._.times(20, (k) => {
+    cy.get('input#addt').type(`todo ${k + 1}{enter}`)
+  })
+  cy.get('[data-cy=todo]').should('have.length', 20)
   cy.screenshot('00-one-todo', { overwrite: true })
   cy.screenshot('01-the-runner', {
     overwrite: true,
