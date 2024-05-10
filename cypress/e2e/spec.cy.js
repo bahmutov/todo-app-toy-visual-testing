dc63b0adb55e602f7bceb482e3477d14aebf2128
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-10 10:52:03 -0400

    set the scroll width

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,10 +2,25 @@
 
 it(
   'takes an image diff of a wide page',
-  { viewportWidth: 1400 },
+  // we are using a small viewport width to simulate locally
+  // the CI situation where the browser total window width is limited
+  { viewportWidth: 700 },
   () => {
-    // load the page and create 20 todos
     cy.visit('/')
+    // the viewport is very narrow, and the page is wider than 700px
+    // to get how much wider the page is, we need to get the scrollWidth
+    // of the body element and set the viewport width to that
+    // Tip: "scrollWidth" is a property of the element
+    // Tip 2: the current viewport height is defined in the config object
+    // https://on.cypress.io/config
+    cy.get('body')
+      .should('have.prop', 'scrollWidth')
+      .should('be.a', 'number')
+      .then((width) => {
+        cy.viewport(width, Cypress.config('viewportHeight'))
+      })
+    // take an image diff of the entire page
+    // do you see the right "Feedback" column?
     cy.imageDiff('wide-page')
   },
 )
