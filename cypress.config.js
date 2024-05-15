c669fcf894f413c14524c2de55624602750fc2c7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-15 11:47:53 -0400

    use high resolution

--- cypress.config.js
+++ cypress.config.js
@@ -8,8 +8,15 @@ module.exports = defineConfig({
   projectId: '8ho7dz',
   fixturesFolder: false,
   e2e: {
+    env: {
+      resolution: 'high',
+    },
     setupNodeEvents(on, config) {
       setupVisualTesting(on, config)
+
+      // https://github.com/bahmutov/cypress-high-resolution
+      require('cypress-high-resolution')(on, config)
+
       return config
     },
     baseUrl: 'http://localhost:3777',
