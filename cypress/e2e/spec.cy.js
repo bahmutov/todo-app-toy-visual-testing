bcca6181f40d7eafe4a2c23ad775ab8e01df887e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-17 18:44:18 -0400

    cypress-if solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="@bahmutov/cypress-toy-visual-testing" />
 
+// https://github.com/bahmutov/cypress-if
+import 'cypress-if'
+
 it('removes the news widget', () => {
   cy.visit('/')
   // hide the "aside.news" element
@@ -7,12 +10,27 @@ it('removes the news widget', () => {
   // Hint: this is a conditional test example
   // https://glebbahmutov.com/cypress-examples/recipes/conditional-testing.html
   cy.get('.news')
+    // using "should(Cypress._.noop)" to avoid failing
+    // even if there is no element at all
     .should(Cypress._.noop)
     .then(($el) => {
-      if (Cypress.dom.isVisible($el)) {
+      if ($el.length && Cypress.dom.isVisible($el)) {
         cy.log('hiding the news widget')
         cy.get('.news').invoke('hide')
       }
     })
+  cy.imageDiff('todo-app')
+})
+
+it('removes the news widget using cypress-if', () => {
+  cy.visit('/')
+  // hide the "aside.news" element
+  // if it is visible
+  // Hint: this is a conditional test example
+  // https://glebbahmutov.com/cypress-examples/recipes/conditional-testing.html
+  cy.get('.news')
+    .if('visible')
+    .invoke('hide')
+    .log('hidden the widget')
   cy.imageDiff('todo-app')
 })
