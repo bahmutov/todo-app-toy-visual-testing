cbd44d8b1a9c86a0abba5cd344197b9ebcea4f45
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-03 13:31:01 -0400

    bonus05

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,11 +5,6 @@ it('looks the same', () => {
   cy.get('main').should('be.visible')
   // get the window's pixel ration and assert it is 1 on Linux
   // or 2 on macOS
-  const expectedPixelRatio =
-    Cypress.platform === 'linux' ? 1 : 2
-  cy.window()
-    .its('devicePixelRatio', { timeout: 0 })
-    .should('equal', expectedPixelRatio)
   // confirm the application looks the same
   cy.imageDiff('todo-app')
 })
