e45797e035bbc26cdc7daef04a9260a937d6051c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-03 13:31:43 -0400

    spelling

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@
 it('looks the same', () => {
   cy.visit('/')
   cy.get('main').should('be.visible')
-  // get the window's pixel ration and assert it is 1 on Linux
+  // get the window's pixel ratio and assert it is 1 on Linux
   // or 2 on macOS
   // confirm the application looks the same
   cy.imageDiff('todo-app')
